0986e95b101163a7ce417f475de6d9e3dcb609dd
Peter Ovchyn <peter@boarlabs.xyz>
2024-10-06 22:52:56 +0300

    Make app framework run

---
+++ veterans/modules/@apostrophecms/home-page/index.js
@@ -0,0 +1,141 @@
+module.exports = {
+    options: {
+      label: 'Home Page'
+    },
+    fields: {
+      add: {
+        topArea: {
+          type: 'area',
+          label: 'Top Area',
+          options: {
+            widgets: {
+              '@apostrophecms/image': {
+                // This option will add these classes to any image
+                className: 'img-fluid image-fit-center-center'
+              },
+              '@apostrophecms/video': {},
+              '@apostrophecms/html': {},
+              '@apostrophecms/rich-text': {
+                toolbar: [
+                  'styles',
+                  '|',
+                  'bold',
+                  'italic',
+                  'strike',
+                  'link',
+                  '|',
+                  'bulletList',
+                  'orderedList',
+                  '|',
+                  'blockquote',
+                  'codeBlock',
+                  '|',
+                  'horizontalRule',
+                  '|',
+                  'undo',
+                  'redo'
+                ],
+                insert: ['table', 'image'],
+                styles: [
+                  {
+                    tag: 'p',
+                    label: 'Paragraph (P)'
+                  },
+                  {
+                    tag: 'h2',
+                    label: 'Heading 2 (H2)'
+                  },
+                  {
+                    tag: 'h2',
+                    label: 'Special underline',
+                    class: 'custom-underline display-6 ps-2'
+                  },
+                  {
+                    tag: 'h3',
+                    label: 'Heading 3 (H3)'
+                  },
+                  {
+                    tag: 'h4',
+                    label: 'Heading 4 (H4)'
+                  }
+                ]
+              },
+              row: {}
+            }
+          }
+        },
+        bottomArea: {
+          type: 'area',
+          label: 'Bottom Area',
+          options: {
+            expanded: true,
+            groups: {
+              core: {
+                label: 'Core Widgets',
+                widgets: {
+                  '@apostrophecms/image': {
+                    // This option will add these classes to any image
+                    className: 'img-fluid image-fit-center-center'
+                  },
+                  '@apostrophecms/video': {},
+                  '@apostrophecms/html': {},
+                  '@apostrophecms/rich-text': {
+                    toolbar: [
+                      'styles',
+                      '|',
+                      'bold',
+                      'italic',
+                      'strike',
+                      'link',
+                      '|',
+                      'bullet_list',
+                      'ordered_list',
+                      '|',
+                      'blockquote',
+                      'code_block',
+                      '|',
+                      'horizontal_rule',
+                      '|',
+                      'undo',
+                      'redo'
+                    ],
+                    styles: [
+                      {
+                        tag: 'p',
+                        label: 'Paragraph (P)'
+                      },
+                      {
+                        tag: 'h2',
+                        label: 'Heading 2 (H2)'
+                      },
+                      {
+                        tag: 'h2',
+                        label: 'Special underline',
+                        class: 'custom-underline display-6 ps-2'
+                      },
+                      {
+                        tag: 'h3',
+                        label: 'Heading 3 (H3)'
+                      },
+                      {
+                        tag: 'h4',
+                        label: 'Heading 4 (H4)'
+                      }
+                    ]
+                  },
+                  row: {}
+                },
+                columns: 2
+              }
+            }
+          }
+        }
+      },
+      group: {
+        basics: {
+          label: 'Basics',
+          fields: [ 'topArea', 'bottomArea' ]
+        }
+      }
+    }
+  };
