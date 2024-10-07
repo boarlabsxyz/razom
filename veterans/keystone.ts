2aee19a4d88c58597c8891c1a72dcf3534594e20
Peter Ovchyn <peter@boarlabs.xyz>
2024-10-07 18:44:38 +0300

    Fix pipeline

--- veterans/keystone.ts
+++ veterans/keystone.ts
@@ -17,11 +17,11 @@ import { withAuth, session } from './auth'
 export default withAuth(
   config({
     db: {
-      // we're using sqlite for the fastest startup experience
+      // we're using PostgreSQL for a more robust database solution
       //   for more information on what database might be appropriate for you
       //   see https://keystonejs.com/docs/guides/choosing-a-database#title
-      provider: 'sqlite',
-      url: 'file:./keystone.db',
+      provider: 'postgresql',
+      url: `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`,
     },
     lists,
     session,
