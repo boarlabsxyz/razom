2e799270a22db7f87c584f3c6fa2eb79fcd0c964
Yuriy <yuramax@gmail.com>
2024-10-20 12:56:16 +0300

    updating command to run only playright tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -75,6 +75,5 @@ export default defineConfig({
   //   command: "npm run start",
   //   url: "http://localhost:3000",
   //   reuseExistingServer: !process.env.CI,
-  //   timeout: 120000,
   // },
 });
