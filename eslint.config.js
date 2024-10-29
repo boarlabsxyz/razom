9a6fbe3372bede6ae90288bb05135c90f8083d8a
Yuriy <yuramax@gmail.com>
2024-10-29 10:18:58 +0200

    fixing issue with comma which was conflict between ESLint and Prettier; additional formatting scripts for package.json

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import playwright from 'eslint-plugin-playwright';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default [
+  {
+    ...playwright.configs['flat/recommended'],
+    files: ['e2e/**'],
+    extends: [
+      'eslint:recommended',
+      'plugin:prettier/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@eslint-plugin-playwright'
+    ],
+    plugins: ['prettier'],
+    rules: {
+      ...playwright.configs['flat/recommended'].rules,
+      'prettier/prettier': 'error',
+      'comma-dangle': [
+        'error',
+        {
+          arrays: 'always-multiline',
+          objects: 'always-multiline',
+          imports: 'always-multiline',
+          exports: 'always-multiline',
+          functions: 'never'
+        }
+      ]
+    },
+    eslintConfigPrettier
+  }
+];
