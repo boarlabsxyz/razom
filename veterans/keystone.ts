5c7c4c517e0cf160b848e03425628e60ab19f428
Vitalii Pikozh <wisart@gmail.com>
2024-12-14 13:57:00 +0200

    fix keystone.ts

--- veterans/keystone.ts
+++ veterans/keystone.ts
@@ -1,27 +1,14 @@
-// Welcome to Keystone!
-//
-// This file is what Keystone uses as the entry-point to your headless backend
-//
-// Keystone imports the default export of this file, expecting a Keystone configuration object
-//   you can find out more at https://keystonejs.com/docs/apis/config
-
 import { config } from '@keystone-6/core';
 
-// to keep this file tidy, we define our schema in a different file
 import { lists } from './schema';
 
-// authentication is configured separately here too, but you might move this elsewhere
-// when you write your list-level access control functions, as they typically rely on session data
 import { withAuth, session } from './auth';
 
 export default withAuth(
   config({
     db: {
-      // we're using PostgreSQL for a more robust database solution
-      //   for more information on what database might be appropriate for you
-      //   see https://keystonejs.com/docs/guides/choosing-a-database#title
       provider: 'postgresql',
-      url: `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`,
+      url: process.env.DATABASE_URL as string,
     },
     lists,
     session,
