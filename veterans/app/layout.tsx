d789817426f5121f538f335a40f0d088f2d2a1bf
Yuriy <yuramax@gmail.com>
2024-12-17 11:27:51 +0200

    adding google fonts using next.js

--- veterans/app/layout.tsx
+++ veterans/app/layout.tsx
@@ -1,4 +1,21 @@
 import React from 'react';
+import { Lato, Golos_Text } from '@next/font/google';
+import styles from '../styles/global.css';
+
+const lato = Lato({
+  weight: ['400'],
+  style: ['normal', 'italic'],
+  variable: '--font-lato',
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+const golosText = Golos_Text({
+  weight: ['400', '500', '600'],
+  variable: '--font-golos-text',
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export const metadata = {
   title: 'Razom for veterans',
@@ -12,7 +29,7 @@ export default function RootLayout({
   readonly children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${lato.variable} ${golosText.variable}`}>
       <body>{children}</body>
     </html>
   );
