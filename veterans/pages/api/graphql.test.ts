0fe3bb4865b10f99828eba6f7272a12b560fd7aa
Vitalii Pikozh <wisart@gmail.com>
2024-12-18 16:13:59 +0200

    fix graphql.test

--- veterans/pages/api/graphql.test.ts
+++ veterans/pages/api/graphql.test.ts
@@ -1,77 +1,30 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { createYoga } from 'graphql-yoga';
-import { keystoneContext } from '../../keystone/context';
-import { GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
+import { makeExecutableSchema } from '@graphql-tools/schema';
 
-jest.mock('graphql-yoga', () => ({
-  createYoga: jest.fn(),
-}));
+const mockWithRequest = jest.fn(() => ({ user: 'mock-user' }));
+jest.mock('../../keystone/context', () => {
+  const typeDefs = `
+      type Query {
+        _empty: String
+        }
+        `;
+  const mockSchema = makeExecutableSchema({ typeDefs });
 
-jest.mock('../../keystone/context', () => ({
-  keystoneContext: {
-    graphql: { schema: {} },
-    withRequest: jest.fn(),
-  },
-}));
-
-describe('GraphQL API Route', () => {
-  let mockRequest: Partial<NextApiRequest>;
-  let mockResponse: Partial<NextApiResponse>;
-
-  beforeEach(() => {
-    mockRequest = {
-      method: 'POST',
-      body: '{}',
-      headers: { 'content-type': 'application/json' },
-    };
-    mockResponse = {
-      statusCode: 200,
-      json: jest.fn().mockResolvedValue({ data: 'mocked-data' }),
-    };
-
-    (createYoga as jest.Mock).mockImplementation(({ context }) => {
-      return (req: NextApiRequest, res: NextApiResponse) => {
-        context({ req, res });
-        res.statusCode = 200;
-        res.json({ data: 'mocked-data' });
-      };
-    });
-  });
-
-  it('should create a Yoga API with keystoneContext and handle the request', async () => {
-    const queryType = new GraphQLObjectType({
-      name: 'Query',
-      fields: {
-        hello: {
-          type: GraphQLString,
-          resolve: () => 'Hello, world!',
-        },
+  return {
+    keystoneContext: {
+      graphql: {
+        schema: mockSchema,
       },
-    });
+      withRequest: mockWithRequest,
+    },
+  };
+});
 
-    const schema = new GraphQLSchema({
-      query: queryType,
-    });
+import { keystoneContext } from '../../keystone/context';
+import { config } from './graphql';
 
-    const yogaApi = createYoga({
-      graphqlEndpoint: '/api/graphql',
-      schema,
-      context: ({ req, res }: { req: NextApiRequest; res: NextApiResponse }) =>
-        keystoneContext.withRequest(req, res),
-    });
-
-    await yogaApi(
-      mockRequest as NextApiRequest,
-      mockResponse as NextApiResponse,
-    );
-
-    expect(keystoneContext.withRequest).toHaveBeenCalledWith(
-      mockRequest,
-      mockResponse,
-    );
-
-    expect(mockResponse.statusCode).toBe(200);
-
-    expect(mockResponse.json).toHaveBeenCalledWith({ data: 'mocked-data' });
+describe('/api/graphql API handler', () => {
+  it('should have the correct API snd schema configuration', () => {
+    expect(config.api.bodyParser).toBe(false);
+    expect(keystoneContext.graphql.schema).toBeDefined();
   });
 });
