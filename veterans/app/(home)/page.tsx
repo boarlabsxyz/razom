168263e9277d25db1282f4e8de6d69ecceb6677a
Vitalii Pikozh <wisart@gmail.com>
2024-12-23 14:09:34 +0200

    26-implement-work-with-database-for-storing-and-displaying-data (#54)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    	- Introduced a new API route for handling GraphQL requests.
    	- Added dynamic post fetching and rendering on the HomePage.
    	- Updated environment variable placeholder in the example file.
    
    - **Bug Fixes**
    	- Enhanced error handling for database connection setup.
    
    - **Documentation**
    - Updated README instructions for environment setup, including directory
    path for `.env` file.
    
    - **Style**
    	- Modified CSS for the HomePage layout, setting a maximum width.
    
    - **Tests**
    - Added comprehensive tests for HomePage, Keystone context, and GraphQL
    API route.
    
    - **Chores**
    	- Updated dependencies in the package.json file.
    - Adjusted paths in the GitHub Actions workflow for environment file
    handling.
    - Added a new entry to the .gitignore file to ignore the veterans/.env
    file.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- veterans/app/(home)/page.tsx
+++ veterans/app/(home)/page.tsx
@@ -1,11 +1,72 @@
 import st from './page.module.css';
+import { keystoneContext } from '../../keystone/context';
 
-export default function HomePage() {
+type Post = {
+  id: string;
+  title: string;
+  content: {
+    document: {
+      type: string;
+      children: {
+        text: string;
+      }[];
+    }[];
+  };
+};
+
+type ProcessedPost = {
+  id: string;
+  title: string;
+  content: string | null;
+};
+
+export default async function HomePage() {
+  const posts = Array.from(
+    await keystoneContext.query.Post.findMany({
+      query: `
+      id
+      title
+      content {
+        document
+      }
+    `,
+    }),
+  ) as Post[];
+
+  const processedPosts: ProcessedPost[] = posts.map((post) => {
+    try {
+      return {
+        ...post,
+        content:
+          post.content?.document
+            .map((paragraph) =>
+              paragraph.children.map((child) => child.text).join(' '),
+            )
+            .join('\n') ?? null,
+      };
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(`Error processing post ${post.id}:`, error);
+      return { ...post, content: null };
+    }
+  });
+
   return (
     <main className={st.container}>
       <div>
         <h1>Coming soon...</h1>
         <p>The site is under construction</p>
+        <p>Test posts:</p>
+        <ul>
+          {processedPosts.map((post) => (
+            <li key={post.id}>
+              <div>
+                <h3>{post.title}</h3>
+                <p>{post.content}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     </main>
   );
