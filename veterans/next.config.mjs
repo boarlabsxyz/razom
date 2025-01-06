86082f3a316b5d48f034d9efcc858e86c0aab8fb
Vitalii Pikozh <wisart@gmail.com>
2025-01-06 20:51:11 +0200

    test

--- veterans/next.config.mjs
+++ veterans/next.config.mjs
@@ -2,14 +2,4 @@ import withPreconstruct from '@preconstruct/next';
 
 export default withPreconstruct({
   serverExternalPackages: ['graphql'],
-  // webpack(config) {
-  //   config.externals = {
-  //     ...config.externals,
-  //     'styled-jsx': 'styled-jsx',
-  //     // eslint-disable-next-line prettier/prettier
-  //     next: 'next',
-  //     '@swc/helpers': '@swc/helpers',
-  //   };
-  //   return config;
-  // },
 });
