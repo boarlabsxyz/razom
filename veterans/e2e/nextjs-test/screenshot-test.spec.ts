c58ffe640de668ffad8d3526bcfa80b2d296dfdf
Ihor Masechko <110039829+IhorMasechko@users.noreply.github.com>
2025-02-03 14:08:33 +0200

    #77 Create e2e tests on creating front-end snapshots for Razom (#83)
    Added Playwright screenshot test

---
+++ veterans/e2e/nextjs-test/screenshot-test.spec.ts
@@ -0,0 +1,30 @@
+import { test } from '@playwright/test';
+
+test('Snapshot for Home Page without Hero Section', async ({ page }) => {
+  await page.goto(process.env.BASE_URL || 'http://localhost:8000/', {
+    timeout: 60000,
+    waitUntil: 'load',
+  });
+
+  const heroSection = await page.waitForSelector(
+    'section[data-test-id="blog-initiatives"]',
+    { timeout: 5000 },
+  );
+
+  if (heroSection) {
+    await page.evaluate(() => {
+      const hero = document.querySelector(
+        'section[data-test-id="blog-initiatives"]',
+      );
+      if (hero) {
+        hero.remove();
+      }
+    });
+  }
+
+  await page.waitForLoadState('load');
+  await page.setViewportSize({ width: 1280, height: 720 });
+  await page.screenshot({ fullPage: true });
+
+  // expect(snapshot).toMatchSnapshot('homepage-no-hero.png');
+});
