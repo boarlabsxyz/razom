c655b3e48c580e8bf66def99d55f1c5ba7922784
Anton Kovalchuk <79926879+Anton-88@users.noreply.github.com>
2025-02-12 16:02:21 +0200

    Added spinner as a reusable component (#89)
    - **New Features**
    - Replaced the static loading message with an animated spinner on the
    home page for a smoother loading experience.
    - Introduced a new `Spinner` component for enhanced loading state
    presentation.
    
    - **Style**
      - Refined footer styling for improved layout and responsiveness.
    - Added new styling for the spinner component to ensure proper
    alignment.
    
    - **Chores**
      - Updated `CHANGELOG.md` to document notable changes in the project.
    - Enhanced dependency management with the addition of the
    `react-spinners` library.

---
+++ veterans/components/common/spinner/spinner.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react';
+import BeatLoader from 'react-spinners/BeatLoader';
+
+import st from '@comComps/spinner/spinner.module.css';
+
+interface IProps {
+  loading?: boolean;
+  color?: string;
+}
+
+const Spinner: FC<IProps> = ({
+  loading = true,
+  color = 'var(--fiji-green)',
+}) => (
+  <div className={st.wrapper}>
+    <BeatLoader
+      color={color}
+      loading={loading}
+      size={22}
+      margin={8}
+      speedMultiplier={0.7}
+      aria-label="Loading Spinner"
+      data-testid="loader"
+    />
+  </div>
+);
+
+export default Spinner;
