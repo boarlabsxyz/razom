790596399ca31b43daac8aecb9c3955664051df0
Vitalii Pikozh <wisart@gmail.com>
2025-02-14 01:57:42 +0200

    test

--- scripts/wait-schema-for-nextjs.sh
+++ scripts/wait-schema-for-nextjs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Checking if schema.prisma exists..."
 while [ ! -f /usr/src/app/shared/schema.prisma ]; do
-    echo "schema.prisma not found, retrying in 20 seconds..."
-    sleep 20
+    echo "schema.prisma not found, retrying in 5 seconds..."
+    sleep 5
 done
 echo "schema.prisma found!"
