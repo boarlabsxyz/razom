4d7a957fdd16f078c54f6f0b083549bdaddb4de6
Vitalii Pikozh <wisart@gmail.com>
2025-02-13 14:07:30 +0200

    fix schema.ts

--- veterans/schema.ts
+++ veterans/schema.ts
@@ -1,13 +1,10 @@
-import type { Lists } from '.keystone/types';
 import { User } from './keystone/models/User';
 import { Initiative } from './keystone/models/Initiative';
-import { Category } from './keystone/models/CommonTaxonomyModel';
-import { Source } from './keystone/models/CommonTaxonomyModel';
-import { Session } from './keystone/access';
+import { Category, Source } from './keystone/models/CommonTaxonomyModel';
 
 export const lists = {
   User,
   Initiative,
   Category,
   Source,
-} satisfies Lists<Session>;
+};
