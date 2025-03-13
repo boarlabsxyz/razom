0f6e59ff1a6dbee8a943fde3e97023a23929dde0
Anton Kovalchuck <antonkovalchuck@gmail.com>
2025-03-13 13:47:05 +0200

    removed duplicated list

--- veterans/app/layout.tsx
+++ veterans/app/layout.tsx
@@ -8,7 +8,6 @@ import Banner from '@comComps/header/banner';
 import Footer from '@comComps/footer/Footer';
 import FooterContent from '@comComps/footer/FooterContent';
 import ApolloProviderWrapper from '@comps/apolloProvider';
-import RegionsList from '@comps/homePage/initiatives/regionsFilter/regionsList';
 
 const lato = Lato({
   weight: ['300', '400', '700'],
@@ -50,10 +49,7 @@ export default function RootLayout({
               <Banner name="logotype" height={34} />
             </HeaderContent>
           </Header>
-          <main>
-            {children}
-            <RegionsList />
-          </main>
+          <main>{children}</main>
           <Footer>
             <FooterContent>
               <Banner name="logotype" height={34} />
