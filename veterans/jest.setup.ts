4078026a99f0c6fc03461c8d942d65b5fd872d53
Vitalii Pikozh <wisart@gmail.com>
2025-03-17 09:42:15 +0200

    fix

--- veterans/jest.setup.ts
+++ veterans/jest.setup.ts
@@ -1,19 +1,10 @@
-// import {
-//   TextEncoder as NodeTextEncoder,
-//   TextDecoder as NodeTextDecoder,
-// } from 'util';
-
-// global.TextEncoder = NodeTextEncoder as typeof TextEncoder;
-// global.TextDecoder = NodeTextDecoder as typeof TextDecoder;
-
+import fetchMock from 'jest-fetch-mock';
 import {
   TextEncoder as NodeTextEncoder,
   TextDecoder as NodeTextDecoder,
 } from 'util';
 
-// Перевизначення глобальних змінних TextEncoder і TextDecoder для тестів
 global.TextEncoder = NodeTextEncoder as typeof TextEncoder;
 global.TextDecoder = NodeTextDecoder as typeof TextDecoder;
 
-import fetchMock from 'jest-fetch-mock';
 fetchMock.enableMocks();
