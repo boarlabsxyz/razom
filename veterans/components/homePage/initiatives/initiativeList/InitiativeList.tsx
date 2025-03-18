53e7aec955d93ce93fa572e84e42926cf69dda40
Vitalii Pikozh <wisart@gmail.com>
2025-03-18 11:12:09 +0200

    refactor InitiaveList and create initiativeUtils.test

--- veterans/components/homePage/initiatives/initiativeList/InitiativeList.tsx
+++ veterans/components/homePage/initiatives/initiativeList/InitiativeList.tsx
@@ -5,9 +5,10 @@ import { useMemo } from 'react';
 
 import Spinner from '@comComps/spinner';
 
-import { Initiative, ProcessedInitiative, Paragraph, Child } from 'types';
+import { Initiative } from 'types';
 
 import st from './InitiativeList.module.css';
+import { processInitiatives } from 'utils/initiativeUtils';
 
 const GET_INITIATIVES = gql`
   query GetInitiatives {
@@ -20,32 +21,6 @@ const GET_INITIATIVES = gql`
     }
   }
 `;
-
-function processInitiatives(data?: {
-  initiatives: Initiative[];
-}): ProcessedInitiative[] {
-  if (!data?.initiatives) {
-    return [];
-  }
-
-  return data.initiatives.map(({ id, title, description }) => ({
-    id,
-    title,
-    description: extractTextFromDocument(description?.document) ?? null,
-  }));
-}
-
-function extractTextFromDocument(document?: Paragraph[]): string | null {
-  if (!document) {
-    return null;
-  }
-
-  return document
-    .map((paragraph) =>
-      paragraph.children.map((child: Child) => child.text).join(' '),
-    )
-    .join('\n');
-}
 
 export default function InitiativesList() {
   const { loading, error, data } = useQuery<{ initiatives: Initiative[] }>(
