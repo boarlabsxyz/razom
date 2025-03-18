9c1a1ea49b77c7405d372de99017bb55538e4c16
Ihor Masechko <110039829+IhorMasechko@users.noreply.github.com>
2025-03-18 13:36:33 +0200

    109 add categories and sources of initiatives as filters with checkboxes (#116)
    Added checkbox UI

---
+++ veterans/components/homePage/mapSection/initiativesFilter/InitiativesFilter.test.tsx
@@ -0,0 +1,38 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { CheckboxGroup } from './InitiativesFilter';
+
+const categoriesList = [
+  { id: 'cat-1', name: 'Test Category 1' },
+  { id: 'cat-2', name: 'Test Category 2' },
+];
+
+describe('CheckboxGroup Component', () => {
+  test('renders title correctly', () => {
+    render(<CheckboxGroup title="Test Title" categories={categoriesList} />);
+    expect(screen.getByText('Test Title')).toBeInTheDocument();
+  });
+
+  test('toggles category list when clicked', () => {
+    render(<CheckboxGroup title="Test Title" categories={categoriesList} />);
+    const titleElement = screen.getByText('Test Title');
+
+    fireEvent.click(titleElement);
+
+    const categoryList = screen.getByTestId('category-list-test-title-content');
+    expect(categoryList).toHaveClass('show');
+
+    fireEvent.click(titleElement);
+    expect(categoryList).not.toHaveClass('show');
+  });
+
+  test('renders checkboxes correctly', () => {
+    render(<CheckboxGroup title="Test Title" categories={categoriesList} />);
+
+    categoriesList.forEach((category) => {
+      const checkbox = screen.getByLabelText(category.name);
+      expect(checkbox).toBeInTheDocument();
+      expect(checkbox).toHaveAttribute('type', 'checkbox');
+    });
+  });
+});
