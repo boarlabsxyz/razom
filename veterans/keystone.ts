df25390b088d432d9aa2d5cbf059162df314d351
Vitalii Pikozh <wisart@gmail.com>
2025-03-24 19:53:46 +0200

    test

--- veterans/keystone.ts
+++ veterans/keystone.ts
@@ -65,9 +65,31 @@ export default withAuth<TypeInfo<Session>>(
   config<TypeInfo>({
     db: {
       provider: 'postgresql',
-      url:
-        process.env.DATABASE_URL ||
-        'postgresql://postgres:postgres@localhost:5432/razom',
+      url: (() => {
+        switch (process.env.NODE_ENV) {
+          case 'production':
+            if (!process.env.PRODUCTION_DATABASE_URL) {
+              throw new Error(
+                'PRODUCTION_DATABASE_URL environment variable is not set',
+              );
+            }
+            return process.env.PRODUCTION_DATABASE_URL;
+          case 'test':
+            if (!process.env.TEST_DATABASE_URL) {
+              throw new Error(
+                'TEST_DATABASE_URL environment variable is not set',
+              );
+            }
+            return process.env.TEST_DATABASE_URL;
+          default:
+            if (!process.env.DEVELOPMENT_DATABASE_URL) {
+              throw new Error(
+                'DEVELOPMENT_DATABASE_URL environment variable is not set',
+              );
+            }
+            return process.env.DEVELOPMENT_DATABASE_URL;
+        }
+      })(),
     },
     lists,
     session: statelessSessions({
