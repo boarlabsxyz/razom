79d3f2275ddfd36bc979b227fc19636b0b19c61a
Vitalii Pikozh <wisart@gmail.com>
2025-04-09 19:13:45 +0300

    test

--- veterans/keystone.ts
+++ veterans/keystone.ts
@@ -1,3 +1,76 @@
+// import { config } from '@keystone-6/core';
+// import { lists } from './schema';
+// import { withAuth, session } from './auth';
+
+// const checkEnvVariables = () => {
+//   if (!process.env.SESSION_SECRET || process.env.SESSION_SECRET.length < 32) {
+//     throw new Error(
+//       'SESSION_SECRET must be set and at least 32 characters long',
+//     );
+//   }
+//   if (!process.env.DATABASE_URL) {
+//     throw new Error('DATABASE_URL must be set');
+//   }
+// };
+
+// checkEnvVariables();
+
+// const databaseUrl = process.env.DATABASE_URL;
+
+// if (!databaseUrl) {
+//   throw new Error('DATABASE_URL environment variable is required');
+// }
+
+// const allowedOrigins = process.env.CORS_ORIGINS
+//   ? process.env.CORS_ORIGINS.split(',')
+//   : ['http://localhost:8000'];
+
+// const vercelPattern = /^https:\/\/razom-.*-kavoon\.vercel\.app$/;
+// const isVercelDeployment = (origin: string) => vercelPattern.test(origin);
+
+// const corsConfig = {
+//   origin: (
+//     origin: string | undefined,
+//     callback: (err: Error | null, allow?: boolean) => void,
+//   ) => {
+//     if (!origin) {
+//       return callback(null, true);
+//     }
+
+//     if (allowedOrigins.includes(origin) || isVercelDeployment(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+//   credentials: true,
+// };
+
+// export default withAuth(
+//   config({
+//     db: {
+//       provider: 'postgresql',
+//       url: databaseUrl,
+//       onConnect: async () => {
+//         // eslint-disable-next-line no-console
+//         console.log('✅ Successfully connected to the database.');
+//       },
+//     },
+//     lists,
+//     session,
+//     server: {
+//       cors: corsConfig,
+//       port: Number(process.env.BACKEND_PORT) || 3000,
+//     },
+//     graphql: {
+//       path: '/api/graphql',
+//       cors: corsConfig,
+//     },
+//   }),
+// );
+
 import { config } from '@keystone-6/core';
 import { lists } from './schema';
 import { withAuth, session } from './auth';
@@ -25,24 +98,8 @@ const allowedOrigins = process.env.CORS_ORIGINS
   ? process.env.CORS_ORIGINS.split(',')
   : ['http://localhost:8000'];
 
-const vercelPattern = /^https:\/\/razom-.*-kavoon\.vercel\.app$/;
-const isVercelDeployment = (origin: string) => vercelPattern.test(origin);
-
 const corsConfig = {
-  origin: (
-    origin: string | undefined,
-    callback: (err: Error | null, allow?: boolean) => void,
-  ) => {
-    if (!origin) {
-      return callback(null, true);
-    }
-
-    if (allowedOrigins.includes(origin) || isVercelDeployment(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: allowedOrigins,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
   credentials: true,
