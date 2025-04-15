f3bad85e01e2e36b395835c3e7c0471be35070aa
Vitalii Pikozh <wisart@gmail.com>
2025-04-15 12:05:49 +0300

    fix

--- veterans/app/(home)/page.tsx
+++ veterans/app/(home)/page.tsx
@@ -3,14 +3,12 @@
 import Container from '@comComps/container';
 import Hero from '@comps/homePage/hero/Hero';
 import MapSection from '@comps/homePage/mapSection';
-import InitiativesList from '@comps/homePage/mapSection/initiativeList';
 
 export default function HomePage() {
   return (
     <Container>
       <Hero />
       <MapSection />
-      <InitiativesList />
     </Container>
   );
 }
